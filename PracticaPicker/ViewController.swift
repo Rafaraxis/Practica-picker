c13167d7c9eba9e266437d89a7194a379bec71ad
Alumno <ulsa@MANNY-BRAZZERS.local>
2018-09-13 13:45:59 -0700

    hola

--- PracticaPicker/ViewController.swift
+++ PracticaPicker/ViewController.swift
@@ -8,11 +8,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+    class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+        
+        @IBOutlet weak var ImagenColor: UIImageView!
+        
+        
+        let colores = ["Rojo","Naranja","Amarillo","Verde","Azul","Morado"]
+        
+        
+        func numberOfComponents(in pickerView: UIPickerView) -> Int {
+            return 1
+        }
+        
+        func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+            return colores.count
+        }
+        func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+            return colores[row]
+        }
+        func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+            
+            switch row {
+            case 0:
+                ImagenColor.image = UIImage(named:"Rojo")
+              
+            case 1:
+                ImagenColor.image = UIImage(named:"Naranja")
+            case 2:
+                ImagenColor.image = UIImage(named:"Amarillo")
+            case 3:
+                ImagenColor.image = UIImage(named:"Verde")
+            case 4:
+               ImagenColor.image = UIImage(named:"Azul")
+            case 5:
+                ImagenColor.image = UIImage(named:"Morado")
+           
+            default: break            }
+        }
+        
+        
+    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = UIColor.red
     }
 
     override func didReceiveMemoryWarning() {
